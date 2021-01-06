0c3a723c5296d68f23f1fb925d8811d9a3931cca
Gonzalez32 <3232rose@gmail.com>
2021-01-06 14:22:13 -0600

    ya

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import StarShipList from '../pages/StarShipLists/StarShipList'
+import StarShipPage from '../pages/StarShipPage/StarShipPage'
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+      </>
+     );
+  }
+}
+ 
+export default App;
---
+++ src/pages/StarShipLists/StarShipList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class StarShipList extends Component {
+  state = {};
+  render() {
+    return <></>;
+  }
+}
+
+export default StarShipList;
